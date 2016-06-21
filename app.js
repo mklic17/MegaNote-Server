0530f40fb9c572daf1c603a84b54c7c89743410c
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-21 00:18:08 -0400

    MegaNote Server

---
+++ app.js
@@ -0,0 +1,102 @@
+require('dotenv').load();
+
+var express = require('express');
+var Note = require('./models/note');
+var bodyParser = require('body-parser');
+
+
+var app = express();
+
+//Middleware
+app.use(function(req, res, next){
+    // Allow CORS
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // ALlow Content-Type header (for JSON payloads)
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  //Allow more HTTP verbs
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+
+  // Continue Processing the request
+  next();
+});
+
+// Body parsing for JSON Post/PUT payloads
+app.use(bodyParser.json());
+
+// READ all notes
+app.get('/', function(req, res){
+  Note.find().sort({updated_at: 'desc'}).then(function(notes){
+    res.json(notes);
+  });
+});
+
+// Read One Note
+app.get('/:id', function(req, res){
+  Note.findOne({
+    _id: req.params.id
+  }).then(function(note){
+    res.json(note);
+  });
+});
+
+// CREATE a note
+app.post('/', function(req, res) {
+  var note = new Note({
+    title: req.body.note.title,
+    body_html: req.body.note.body_html
+  });
+
+  note
+    .save()
+    .then(function(noteData) {
+      res.json({
+        message: 'Successfully created note',
+        note: noteData
+      });
+    });
+});
+
+// Update a Note
+app.put('/:id', function(req, res) {
+  Note.findOne({
+    _id: req.params.id
+  }).then(function(note) {
+    note.title = req.body.note.title;
+    note.body_html = req.body.note.body_html;
+    note.save().then(function(){
+      res.json({
+        message: 'Your changes have been saved.',
+        note: note
+      },
+      function(result) {
+        res.json({ message: 'Aww, cuss!' });
+      });
+    });
+  },
+  function(result) {
+    res.json({ message: 'Aww, cuss!' });
+  });
+});
+
+
+app.delete('/:id', function(req, res) {
+  Note.findOne({
+    _id: req.params.id
+  }).then(function(note) {
+    note.remove().then(function() {
+      res.json({
+        message: 'That note has been deleted.',
+        note: note
+      })
+    });
+  });
+});
+
+
+
+app.listen(3030, function() {
+// console.log('DB: ' + process.env.DB_URI);
+  console.log('Listening on http://localhost:3030...');
+});
