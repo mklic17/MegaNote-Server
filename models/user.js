b1f5e82a53f29c9b150bec02485d8abfa84764ae
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-07-08 09:54:19 -0400

    Add User model.

--- models/user.js
+++ models/user.js
@@ -1,13 +1,27 @@
 var db = require('../config/db');
 
 var userSchema = db.Schema({
-  username: String,
-  password: String,
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now
+  },
+
 });
 
-// userSchema.pre('save', function(next) {
-    this.username = username,
-    this.password = password
-// });
+ userSchema.pre('save', function(next) {
+   this.update_at = Date.now();
+   next();
+ });
 
 var User = db.model('User', userSchema);
+
+module.exports = User;
