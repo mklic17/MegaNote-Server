f90ea3a1bf0c61d41defa258374f1d1deda607a1
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-07-12 14:10:43 -0400

    Cerify the JWT and add user to request.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,5 @@
+var jtt = require('jwtwebtoken');
+var User = require('../models/user');
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
 
@@ -8,10 +10,32 @@ module.exports = (req, res, next) => {
 
   if(token) {
     // verify the token
-    next();
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedPayload) => {
+      if (!decodedPayload) {
+        res.status(401).json({ message: 'Authentication required.' });
+        return;
+      }
+
+      // find the user
+      user
+      .findOne({_id: decodedPayload._id})
+      .then(
+        user => {
+          if(user) {
+            // add the user to the request
+            req.user = user;
+            next();
+          }
+          else {
+            // user was not found
+            res.status(401).json({ message: 'Authentication required.' });
+          }
+        }
+      );
+    });
   }
   else {
-    res.status(401).json({ message: 'Authentication required.' })
+    res.status(401).json({ message: 'Authentication required.' });
   }
 }
 
